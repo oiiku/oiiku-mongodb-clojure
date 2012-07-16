b81fa75df75c59c0ff0fda2bee71b5195ef2a049
August Lilleaas <august@augustl.com>
2012-07-16 21:45:37 +0200

    Using snapshot releases, we're not alpha/beta stable yet.

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
-(defproject oiiku-mongodb "0.1.2"
+(defproject oiiku-mongodb "0.1.0-SNAPSHOT"
   :description "Common utils for MongoDB"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [com.novemberain/monger "1.1.0"]
                  [bultitude "0.1.7"]]
-  :deploy-repositories {"releases" "http://augustl.com:8081/nexus/content/repositories/releases"})
+  :deploy-repositories
+  {"releases" "http://augustl.com:8081/nexus/content/repositories/releases"
+   "snapshots" "http://augustl.com:8081/nexus/content/repositories/snapshots"})

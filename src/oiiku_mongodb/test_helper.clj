7946d2e03dd4955ec63d6d98bae2dc4f31d32987
August Lilleaas <august@augustl.com>
2012-07-18 05:36:39 +0200

    Documenting the volatilensss of test-helper/reset-db.

--- src/oiiku_mongodb/test_helper.clj
+++ src/oiiku_mongodb/test_helper.clj
@@ -3,6 +3,9 @@
             monger.collection))
 
 (defn reset-db
+  "The details of how the database is reset are private. You should not assume
+   that the current implementation won't change in the future, all you should
+   assume is that after running this task, the database is as good as new."
   [conn db]
   (doseq [coll-name (remove
                      (fn [coll-name]
